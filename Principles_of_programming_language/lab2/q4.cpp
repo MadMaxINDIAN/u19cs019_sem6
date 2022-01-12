58ec214224aa0a13d767ae22a970ebb8a8582475
Naman Khater <naman22khater@gmail.com>
2022-01-12 11:11:19 +0530

    Tannebaum book added

---
+++ Principles_of_programming_language/lab2/q4.cpp
@@ -0,0 +1,17 @@
+// An educational institution wishes to maintain a database of its employees. 
+// The database is divided into a number of classes whose hierarchical relationships are shown in the following figure. 
+// The figure also shows the minimum information required for each class. 
+// Specify all classes and define functions to create the database and retrieve individual information as and when required. 
+// The database created does not include educational information of the staff. 
+// It has been decided to add this information to teachers and officers (and not for typists) which will help management in decision making with regard to training, promotions etc. 
+// Add another data class called education that holds two pieces of educational information namely highest qualification in general education and highest professional qualification. 
+// This class should be inherited by the class's teacher and officer.
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
